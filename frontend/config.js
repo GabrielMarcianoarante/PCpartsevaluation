747f2f0fc9f8cb181471d71b7420dd77c545fbd9
zkkas <gabrielpcgamer23@gmail.com>
2026-04-05 21:30:21 -0300

    gi

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // substitui essa URL pela URL do seu backend no Railway
 // ex: https://pc-parts-backend-production.up.railway.app
-window.API_URL = 'https://SEU-BACKEND.up.railway.app'
+window.API_URL = 'https://proactive-dream-production-b06f.up.railway.app'
